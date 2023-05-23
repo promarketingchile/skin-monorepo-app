29e92278a7d92a977d8a5fb22207e41af8b3114e
Francisco Aranda <francisco.aranda@sudo.cl>
2023-05-23 08:49:19 -0400

    fix: scss loading in web/latamwin

--- apps/web/latamwin/app/page.tsx
+++ apps/web/latamwin/app/page.tsx
@@ -1,13 +1,14 @@
 import { toSlug } from '@acme/utils';
 import { PrimaryButton } from 'ui-divergana';
 
+import 'tailwindcss/tailwind.css';
 import 'ui-divergana/src/index.module.scss';
 
 export default function Page() {
 	return (
 		<>
-			<div className='flex w-full justify-center bg-red-900 text-lg'>
-				{toSlug('latamwin-chile')}
+			<div className='flex w-full justify-center text-lg'>
+				<h2>{toSlug('latamwin-chile')}</h2>
 				<PrimaryButton>Botón Primario</PrimaryButton>
 			</div>
 		</>
