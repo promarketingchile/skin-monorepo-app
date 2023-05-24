8205c3ed5b4b404da3c183fb6c3668983205b1e0
Francisco Aranda <francisco.aranda@sudo.cl>
2023-05-24 17:42:12 -0400

    refactor: refactored example component interface to the interface file

--- packages/ui/ui-divergana/src/components/Buttons/PrimaryButton/PrimaryButton.interface.ts
+++ packages/ui/ui-divergana/src/components/Buttons/PrimaryButton/PrimaryButton.interface.ts
@@ -1,1 +1,12 @@
+import { VariantProps } from "class-variance-authority";
 import React from "react";
+import { cvaPrimaryButton } from "./PrimaryButton.utils";
+
+
+export interface IPrimaryButtonProps extends VariantProps<typeof cvaPrimaryButton>, React.HTMLAttributes<HTMLButtonElement> {
+	children: JSX.Element | string,
+	loading?: boolean;
+	disabled?: boolean;
+	prepend?: JSX.Element;
+	append?: JSX.Element;
+}
